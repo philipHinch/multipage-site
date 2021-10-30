38234805460ef01e45094eb01d6b63514f51b979
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-10-30 17:59:48 +0300

    routing lesson

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h2>Contact</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto error architecto aut eligendi placeat sit, quidem esse minima dolore. Id harum recusandae nam quasi explicabo dignissimos perferendis non similique? A dignissimos tenetur, iusto quasi iure velit sint est consequatur id, explicabo deserunt laboriosam obcaecati facere, blanditiis aspernatur provident necessitatibus? Veritatis.</p>
+        </div>
+    );
+}
+
+export default Contact;
